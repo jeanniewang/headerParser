6d700dbfd5ab082c7e16d32deeba1d7d4bd01030
Jeannie Wang <jeannie.wang.jw@gmail.com>
2021-06-03 15:11:41 -0400

    thrid message

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/whoami", function (req, res) {
-  res.json({ipaddress: req.ip});
+  res.json({"ipaddress": req.ip, "language": req.language, "software": req.software});
 });
 
 
